dd91ccfb5a13e9e1d5f69cd1b736f386b4568571
Matias <matibravoneron@gmail.com>
2022-10-10 21:04:37 -0300

    fix code

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const app = new Vue({
   el:'#app',
   data:{
     titulo: 'Lista de tareas',
-    tareas:[],
-    nuevaTarea:''
+    tareas: [],
+    nuevaTarea: ''
   },
   methods:{
     agregarTarea: function(){
@@ -12,23 +12,23 @@ const app = new Vue({
         estado: false
       });
       this.nuevaTarea = '';
-      localStorage.seItem('lista-vue', JSON.stringify(this.tareas))
+      localStorage.seItem('lista-vue', JSON.stringify(this.tareas));
     },
     editarTarea: function(index){
       this.tareas[index].estado = true;
-      localStorage.seItem('lista-vue', JSON.stringify(this.tareas))
+      localStorage.seItem('lista-vue', JSON.stringify(this.tareas));
     },
     eliminar: function(index){
       this.tareas.splice(index, 1);
-      localStorage.seItem('lista-vue', JSON.stringify(this.tareas))
+      localStorage.seItem('lista-vue', JSON.stringify(this.tareas));
     }
   },
   created: function(){
-    let datosDB = JSON.parse(localStorage.getItem('lista-vue'))
+    let datosDB = JSON.parse(localStorage.getItem('lista-vue'));
     if(datosDB === null){
       this.tareas = [];
     }else{
       this.tareas = datosDB;
     }
   }
-})
+});
